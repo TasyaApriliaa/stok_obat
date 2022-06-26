34e786a70974e167b4a49fcd18017f80b53e38e8
TasyaApriliaa <12050123544@students.uin-suska.ac.id>
2022-06-26 22:15:14 +0700

    commit

--- config/koneksi.php
+++ config/koneksi.php
@@ -8,7 +8,7 @@ $dsn ="mysql:host={$dbserver};dbname={$dbname}";
 $connection = null;
 try{
     $connection = new PDO($dsn,$dbuser, $dbpassword);
-    echo "koneksi sukses";
+//    echo "koneksi sukses";
 }catch (Exception $exception){
     die("terjadi error: {$exception->getMessage()}");
 }
